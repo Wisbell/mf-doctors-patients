bff868be179509c108ef22563794685d01b3fd40
Wisbell <westleyisbell@gmail.com>
2017-01-19 17:04:27 -0600

    Updated factory view in order to make links to a doctors patients view

--- app/app.js
+++ app/app.js
@@ -1,5 +1,25 @@
 console.log("app.js loaded")
 
+
+// // Initialize Firebase
+
+// var config = {
+//   apiKey: "AIzaSyCgPcl_OJmb0zkDAH_bOM7eKlkrDTGMoQU",
+//   authDomain: "west-doctors-patients.firebaseapp.com",
+//   databaseURL: "https://west-doctors-patients.firebaseio.com",
+//   storageBucket: "west-doctors-patients.appspot.com",
+//   messagingSenderId: "112885923149"
+// };
+// firebase.initializeApp(config);
+
+// // Get a reference to the database service
+// var db = firebase.database();
+
+// // Add local JSON to firebase
+// firebase.db.ref().set('/data/hospital.json')
+
+// Initalize Angular
+
 var app = angular.module('doctorsApp', ['ngRoute'])
 
 app.config(function($routeProvider, $locationProvider){
@@ -15,6 +35,6 @@ app.config(function($routeProvider, $locationProvider){
       templateUrl: 'partials/patientsView.html'
     })
     .otherwise({
-      redirectTo: 'partials/doctorsView.html'
+      redirectTo: '/'
     })
 })
