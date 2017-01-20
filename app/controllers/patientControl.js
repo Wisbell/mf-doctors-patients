e751ccf983ac47c960b832ffc6fc82e15ce3200c
Wisbell <westleyisbell@gmail.com>
2017-01-20 09:12:11 -0600

    Each doctors patient list is now shown when the doctors name is clicked

--- app/controllers/patientControl.js
+++ app/controllers/patientControl.js
@@ -1,13 +1,20 @@
 app.controller('PatientCtrl', function($scope, $routeParams, PatientFactory){
     console.log('patient list controller')
 
-    $scope.doctorFirstName
-    $scope.doctorLastName
+    // $scope.doctorFirstName =
+    // $scope.doctorLastName
 
     $scope.thisDoctorsPatients = $routeParams.patients
 
     PatientFactory.getPatientList($scope.thisDoctorsPatients)
       .then(function(val){
         console.log("patient list?", val)
+
+        $scope.patientList = val
       })
+
+    // PatientFactory.getDoctorInfo($scope.thisDoctorsPatients)
+    //   .then(function(val){
+    //     console.log("patient doctor info?", val)
+    //   })
 })
--- app/factories/patientFactory.js
+++ app/factories/patientFactory.js
@@ -7,6 +7,14 @@ app.factory('PatientFactory', function($http){
                   console.log("patient factory val", val)
                   return val.data
               })
+            },
+
+            getDoctorInfo: function(doctor_id){
+              return $http.get(`https://west-doctors-patients.firebaseio.com/doctor/${doctor_id}`)
+                .then(function(val){
+                  console.log("patient's doctor's info go get name", val)
+                  return val.data
+                })
             }
           }
 })
